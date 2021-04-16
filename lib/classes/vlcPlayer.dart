853d0416747a2f9892158beb2c729e8a39a7cf6c
Alan Willian Melo <alan.willian.melo1@ibm.com>
2021-04-16 19:08:20 -0300

    VLC Player - Step1

---
+++ lib/classes/vlcPlayer.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_vlc_player/flutter_vlc_player.dart';
+import 'package:portrait/classes/videoStateStream.dart';
+
+class MyVlcPlayer extends StatefulWidget {
+  final String path;
+  final String orientation;
+  final VlcPlayerController vlcController;
+  final VideoStateStream videoStateStream;
+
+  const MyVlcPlayer(
+      {Key key,
+      this.path,
+      this.orientation,
+      this.vlcController,
+      this.videoStateStream})
+      : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _MyVlcPlayerState();
+}
+
+class _MyVlcPlayerState extends State<MyVlcPlayer> {
+  VlcPlayerController _vlcPlayerController;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    _vlcPlayerController = widget.vlcController;
+    playing();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return VlcPlayer(
+      aspectRatio: 16 / 9,
+      controller: _vlcPlayerController,
+      placeholder: Center(child: CircularProgressIndicator()),
+    );
+  }
+
+  playing() {
+    _vlcPlayerController.addOnInitListener(() async {
+      bool playing = true;
+      var position1;
+      var position2;
+      while (playing) {
+        await Future.delayed(Duration(milliseconds: 1000));
+        position2 = position1;
+        position1 = await _vlcPlayerController.getPosition().catchError((onError){
+          if(onError.toString().contains('getTime()\' on a null object reference')){
+            print('Vídeo pulado antes do fim');
+          }
+        });
+        if (position2 == position1) {
+          playing = false;
+          print('Video ended in VLC Player');
+          widget.videoStateStream.updateVideoState.add('${widget.path} done');
+        }
+      }
+    });
+  }
+}
