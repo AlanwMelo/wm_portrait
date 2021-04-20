e6a642d5252166366f4dfab23c5d2195266c86ed
[AlanwMelo] <[amelo967@gmail.com]>
2021-04-20 17:12:55 -0300

    ajustes no controle

--- lib/classes/vlcPlayer.dart
+++ lib/classes/vlcPlayer.dart
@@ -24,6 +24,13 @@ class MyVlcPlayer extends StatefulWidget {
 class _MyVlcPlayerState extends State<MyVlcPlayer> {
   VlcPlayerController _vlcPlayerController;
 
+  /// Controles de exibição do painel de controle
+  Widget videoControls;
+  bool showControls = false;
+
+  // Som
+  bool soundOn = false;
+
   @override
   dispose() {
     /// Encerra corretamente o vídeo
@@ -36,7 +43,14 @@ class _MyVlcPlayerState extends State<MyVlcPlayer> {
     // TODO: implement initState
     _vlcPlayerController = widget.vlcController;
     playingControl();
+    videoControls = Container();
     super.initState();
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    videoControls = videoControlBar();
   }
 
   @override
@@ -44,32 +58,38 @@ class _MyVlcPlayerState extends State<MyVlcPlayer> {
     return Container(
       child: Stack(
         children: [
-          Container(
-            height: MediaQuery.of(context).size.height,
-            child: VlcPlayer(
-              aspectRatio: 16 / 9,
-              controller: _vlcPlayerController,
-              placeholder: Center(child: CircularProgressIndicator()),
-            ),
-          ),
           GestureDetector(
-            onTap: () {
-              setState(() {
-                _vlcPlayerController.pause();
-              });
-            },
             child: Container(
               height: MediaQuery.of(context).size.height,
               width: MediaQuery.of(context).size.width,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Container(
-                    height: 100,
-                    color: Colors.green,
-                  )
-                ],
+              child: GestureDetector(
+                onTap: () {
+                  print('tap');
+                  showControls = !showControls;
+                  if (showControls) {
+                    videoControls = videoControlBar();
+                  } else {
+                    videoControls = Container();
+                  }
+                  setState(() {});
+                },
+                child: VlcPlayer(
+                  aspectRatio: 16 / 9,
+                  controller: _vlcPlayerController,
+                  placeholder: Center(child: CircularProgressIndicator()),
+                ),
               ),
+            ),
+          ),
+          Container(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                AnimatedContainer(
+                  duration: Duration(milliseconds: 300),
+                  child: videoControls,
+                )
+              ],
             ),
           ),
         ],
@@ -85,19 +105,20 @@ class _MyVlcPlayerState extends State<MyVlcPlayer> {
 
       print(videoDuration);
       while ((videoDuration.inSeconds - videoPosition.inSeconds) > 1) {
-        if(timeToBreak){
+        if (timeToBreak) {
           print('Time to Break');
           break;
         }
         var durationHelper =
             await _vlcPlayerController.getDuration().catchError((onError) {
-              print('<<<<<<<<<<< $onError');
-
+          print('<<<<<<<<<<< $onError');
         });
         var positionHelper =
             await _vlcPlayerController.getPosition().catchError((onError) {
           print('>>>>>>>>>>>>>>>>. $onError');
-          if (onError.toString().contains('was called on a disposed VlcPlayerController')) {
+          if (onError
+              .toString()
+              .contains('was called on a disposed VlcPlayerController')) {
             timeToBreak = true;
           }
         });
@@ -116,7 +137,62 @@ class _MyVlcPlayerState extends State<MyVlcPlayer> {
   }
 
   Future<void> vlcControllerDisposer() async {
-    await widget.vlcController.stopRendererScanning().catchError((onError){});
+    await widget.vlcController.stopRendererScanning().catchError((onError) {});
     await widget.vlcController.dispose();
+  }
+
+  Widget videoControlBar() {
+    return Container();
+    /*return Container(
+      padding: EdgeInsets.only(right: 8, left: 8),
+      height: 50,
+      color: Colors.black.withOpacity(0.5),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          GestureDetector(
+            onTap: () {
+              _vlcPlayerController.pause();
+            },
+            child: Container(
+              child: Icon(
+                Icons.play_circle_outline,
+                color: Colors.white,
+                size: 35,
+              ),
+            ),
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width * 0.75,
+            child: Slider(
+              value: 50,
+              min: 0,
+              max: 100,
+              onChanged: (newValue) {},
+            ),
+          ),
+          GestureDetector(
+            onTap: () {
+              print('????');
+              soundOn = !soundOn;
+              print(soundOn);
+              setState(() {});
+            },
+            child: Container(
+                child: soundOn == true
+                    ? Icon(
+                        Icons.volume_up,
+                        color: Colors.white,
+                        size: 35,
+                      )
+                    : Icon(
+                        Icons.volume_off,
+                        color: Colors.white,
+                        size: 35,
+                      )),
+          ),
+        ],
+      ),
+    );*/
   }
 }
