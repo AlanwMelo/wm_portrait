8c0bea2f67f74772f636d6079542c2abced1612f
AlanwMelo <amelo967@gmail.com>
2021-11-03 18:26:15 -0300

    Swipper implementado

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,11 @@
+import 'package:card_swiper/card_swiper.dart';
+import 'package:file_manager/controller/file_manager_controller.dart';
+import 'package:file_manager/file_manager.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:portrait/classes/appColors.dart';
+import 'package:portrait/classes/directoryManager.dart';
 import 'package:portrait/colorScreen.dart';
 import 'package:portrait/db/dbManager.dart';
 import 'dart:io';
@@ -30,86 +36,174 @@ class _MyAppState extends State<MyApp> {
         accentColor: AppColorsDialga().accentColor(),
         scaffoldBackgroundColor: AppColorsDialga().scaffoldBackGround(),
       ),
-      home: MyHomePage(title: 'WM Portrait'),
+      home: _MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-  final String title;
+class _MyHomePage extends StatefulWidget {
+  _MyHomePage({Key? key}) : super(key: key);
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<_MyHomePage>
+    with SingleTickerProviderStateMixin {
   String? testeIMG;
-  int _counter = 0;
+  int animatedTextIndex = 0;
 
-  Future<void> _incrementCounter() async {
-    setState(() {
-      _counter++;
-    });
+  // List with all directories that contains images or videos
+  List<Directory> usableDirectories = [];
+
+  // Animated Text color controllers
+  late Animation<Color?> photoAnimation;
+  late Animation<Color?> albumAnimation;
+  late AnimationController colorAnimationController;
+
+  SwiperController swiperController = SwiperController();
+
+  @override
+  void initState() {
+    _initTextControllers();
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text(widget.title),
-      ),
-      body: Container(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              GestureDetector(
-                onTap: () {
-                 /* Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => OpenList(
-                              listName: 'thisList',
-                              appName: widget.title,
-                              )));*/
-                },
-                child: Container(
-                  height: 50,
-                  color: Colors.limeAccent,
-                  child: Text("test"),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.all(15),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          GestureDetector(
+                            onTap: () {
+                              colorAnimationController.reverse();
+                              swiperController.move(0);
+                            },
+                            child: _topText('Photos', photoAnimation.value),
+                          ),
+                          GestureDetector(
+                            onTap: () {
+                              colorAnimationController.forward();
+                              swiperController.move(1);
+                            },
+                            child: _topText('Albums', albumAnimation.value),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  GestureDetector(
+                      onTap: () {
+                        print('ayaya');
+                      },
+                      child: Container(child: Icon(Icons.more_vert_rounded))),
+                ],
+              ),
+            ),
+            Expanded(
+              child: Container(
+                child: NotificationListener<OverscrollIndicatorNotification>(
+                  //Removes glow animation on overscroll
+                  onNotification:
+                      (OverscrollIndicatorNotification? overscroll) {
+                    overscroll!.disallowGlow();
+                    return true;
+                  },
+                  child: Swiper(
+                      controller: swiperController,
+                      loop: false,
+                      itemCount: 2,
+                      onIndexChanged: (index) {
+                        index == 0
+                            ? colorAnimationController.reverse()
+                            : colorAnimationController.forward();
+                      },
+                      itemBuilder: (BuildContext context, int index) {
+                        return Container(
+                          child: Container(
+                            child: Center(
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: <Widget>[
+                                  GestureDetector(
+                                    onTap: () async {
+                                      DirectoryManager()
+                                          .getDirectoriesWithImagesAndVideos(
+                                              (answer) {
+                                        answer.forEach((element) {
+                                          print(element);
+                                        });
+                                      });
+                                    },
+                                    child: Container(
+                                      height: 50,
+                                      color: Colors.limeAccent,
+                                      child: Text("test"),
+                                    ),
+                                  ),
+                                  GestureDetector(
+                                    onTap: () {
+                                      Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                              builder: (context) =>
+                                                  ColorScreen()));
+                                    },
+                                    child: Container(
+                                      height: 50,
+                                      color: Colors.limeAccent,
+                                      child: Text("Colors"),
+                                    ),
+                                  ),
+                                  testeIMG == null
+                                      ? Container()
+                                      : Container(
+                                          child:
+                                              Image.file(new File(testeIMG!))),
+                                ],
+                              ),
+                            ),
+                          ),
+                        );
+                      }),
                 ),
               ),
-              GestureDetector(
-                onTap: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => ColorScreen()));
-                },
-                child: Container(
-                  height: 50,
-                  color: Colors.limeAccent,
-                  child: Text("Colors"),
-                ),
-              ),
-              testeIMG == null
-                  ? Container()
-                  : Container(child: Image.file(new File(testeIMG!))),
-              Text(
-                '$_counter',
-                style: Theme.of(context).textTheme.headline4,
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  _initTextControllers() {
+    colorAnimationController = AnimationController(
+        duration: const Duration(milliseconds: 400), vsync: this);
+    albumAnimation = ColorTween(begin: Colors.black, end: Colors.blueAccent)
+        .animate(colorAnimationController)
+      ..addListener(() {
+        setState(() {});
+      });
+    photoAnimation = ColorTween(begin: Colors.blueAccent, end: Colors.black)
+        .animate(colorAnimationController)
+      ..addListener(() {
+        setState(() {});
+      });
+  }
+
+  _topText(String s, Color? color) {
+    return Text(
+      s,
+      style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold, color: color),
     );
   }
 }
